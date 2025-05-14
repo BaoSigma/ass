a8984970c379f99bb3b6ebfc78939af6317566c4
BaoSigma <baohakura@gmail.com>
2025-05-14 15:45:36 +0700

    Ass

---
+++ src/poly/cafe/dao/impl/DoUongimpl.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poly.cafe.dao.impl;
+
+import poly.cafe.util.XJdbc;
+
+/**
+ *
+ * @author baoha
+ */
+public class DoUongimpl {
+    private String sql = "INSERT INTO DoUong(maDU, tenDU,anhDoUong,size,maLDU"
+}
