2231a630e0ea9757b118ec1e2e83e21df133b3e4
dongkhanh208 <khanhdgtv00309@gmail.com>
2025-05-15 21:27:14 +0700

    code

---
+++ src/poly/cafe/dao/entityDAO/NguoiDungDAO.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package poly.cafe.dao.entityDAO;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import src.poly.cafe.dao.CRUDDAO;
+import poly.cafe.entity.NguoiDung;
+
+/**
+ *
+ * @author micro
+ */
+public interface NguoiDungDAO extends CRUDDAO<NguoiDung, Object> {    
+    List<NguoiDung> list = new ArrayList<>();
+    @Override
+    public default NguoiDung create(NguoiDung nd){
+        list.add(nd);
+        return nd;
+    }
+    
+    @Override
+    public default void deleteById(Object id){
+        list.removeIf(Pbh -> Pbh.getMaND().equals(id));
+    }
+    
+    @Override
+    public default void update(NguoiDung entity){
+        for (int i = 0; i < list.size(); i++){
+            if(list.get(i).getMaND().equals(entity.getMaND())){
+                list.set(i, entity);
+                return;
+            }
+        }
+    }
+    
+    @Override
+    public default List<NguoiDung> findAll(){
+        return list;
+    }
+    
+    @Override
+    public default NguoiDung findById(Object id) {
+        Optional<NguoiDung> NguoiDung = list.stream().filter(Pbh -> Pbh.getMaND().equals(id)).findFirst();
+        return NguoiDung.orElse(null);
+        }
+    }
