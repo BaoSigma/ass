4c5c5a1427bb2576db7e52bc366bf102764b072c
BaoSigma <baohakura@gmail.com>
2025-05-17 22:54:39 +0700

    Ass

---
+++ src/poly/cafe/controller/entityController/LoaiDoUongController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package poly.cafe.controller.entityController;
+
+import poly.cafe.controller.CRUDCTRL;
+
+/**
+ *
+ * @author baoha
+ */
+public interface LoaiDoUongController extends CRUDCTRL<Object>{
+    
+}
