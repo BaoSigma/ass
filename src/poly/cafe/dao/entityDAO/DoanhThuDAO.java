e8e2b88309d23c9a3fce46945bf0be0658396723
BaoSigma <baohakura@gmail.com>
2025-05-19 16:05:57 +0700

    Ass

--- src/poly/cafe/dao/entityDAO/DoanhThuDAO.java
+++ src/poly/cafe/dao/entityDAO/DoanhThuDAO.java
@@ -14,37 +14,6 @@ import src.poly.cafe.dao.CRUDDAO;
  *
  * @author baoha
  */
-public interface DoanhThuDAO extends CRUDDAO<DoanhThu, Object>{
+public interface DoanhThuDAO extends    CRUDDAO<DoanhThu, Object>{
     List<DoanhThu> list = new ArrayList<>();
-    @Override
-    public default DoanhThu create(DoanhThu Ldu) {
-        list.add(Ldu);
-        return Ldu;
-    }
-
-    @Override
-    public default void deleteById(Object id) {
-        list.removeIf(ldu -> ldu.getMaDT().equals(id));
-    }
-
-    @Override
-    public default void update(DoanhThu entity) {
-        for (int i = 0; i < list.size(); i++) {
-            if(list.get(i).getMaDT().equals(entity.getMaDT())){
-                list.set(i, entity);
-                return;
-            }
-        }
-    }
-
-    @Override
-    public default List<DoanhThu> findAll() {
-        return list;
-    }
-
-    @Override
-    public default DoanhThu findById(Object id) {
-        Optional<DoanhThu> LoaiDoUong = list.stream().filter(ldu -> ldu.getMaDT().equals(id)).findFirst();
-        return LoaiDoUong.orElse(null);
-    }
 }
