9031df7bd043b50b32a884b8cbf533c6c389fd88
ExpertBook <ExpertBook@LAPTOP-VA7RJ4L7>
2025-05-28 15:42:08 +0700

    tao la khoa

---
+++ src/poly/cafe/entity/theDD.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poly.cafe.entity;
+
+/**
+ *
+ * @author ExpertBook
+ */
+public class theDD {
+    private int ID;
+    private String trangThai;
+    private  HoaDon hd =  new HoaDon();
+
+    public theDD(int ID, String trangThai) {
+        this.ID = ID;
+        this.trangThai = trangThai;
+    }
+
+    public theDD() {
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    public HoaDon getHd() {
+        return hd;
+    }
+
+    public void setHd(HoaDon hd) {
+        this.hd = hd;
+    }
+
+}
