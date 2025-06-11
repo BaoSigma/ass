1f9915fc4cf13eef90c631e5d3d6eb4d78a21306
BaoSigma <baohakura@gmail.com>
2025-06-11 10:53:03 +0700

    7TDT

---
+++ src/poly/cafe/dao/entityDAO/DoanhThuDAO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package poly.cafe.dao.entityDAO;
+
+import java.util.List;
+import poly.cafe.entity.ChiTietHoaDon;
+import poly.cafe.entity.HoaDon;
+import poly.cafe.entity.NhanVien;
+import poly.cafe.entity.SanPham;
+
+/**
+ *
+ * @author baoha
+ */
+public interface DoanhThuDAO{
+    List<ChiTietHoaDon> TongDoanhThu();
+    List<HoaDon> DoanhThuTheoNgay();
+    List<NhanVien> DoanhThuNhanVien();
+    
+}
---
+++ src/poly/cafe/dao/impl/DoanhThuimpl.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poly.cafe.dao.impl;
+
+import java.util.List;
+import poly.cafe.dao.entityDAO.DoanhThuDAO;
+import poly.cafe.entity.ChiTietHoaDon;
+import poly.cafe.entity.HoaDon;
+import poly.cafe.entity.NhanVien;
+import poly.cafe.util.XQuery;
+
+/**
+ *
+ * @author baoha
+ */
+public class DoanhThuimpl implements DoanhThuDAO{
+    String TongDoanhThuSQL = "SELECT SUM(soLuong * giaTien) AS TongDoanhThu FROM Chitiethoadon";
+    String DoanhThuTheoNgaySQL = "SELECT \n" +
+"    HD.ngayTao,\n" +
+"    SUM(CT.soLuong * CT.giaTien) AS DoanhThuTheoNgay\n" +
+"FROM \n" +
+"    Hoadon HD\n" +
+"JOIN \n" +
+"    Chitiethoadon CT ON HD.maHD = CT.maHD\n" +
+"GROUP BY \n" +
+"    HD.ngayTao\n" +
+"ORDER BY \n" +
+"    HD.ngayTao";
+    String DoanhThuNhanVienSQL="SELECT \n" +
+"    NV.hoTen,\n" +
+"    NV.maNV,\n" +
+"    SUM(CT.soLuong * CT.giaTien) AS DoanhThuTheoNhanVien\n" +
+"FROM \n" +
+"    Hoadon HD\n" +
+"JOIN \n" +
+"    Chitiethoadon CT ON HD.maHD = CT.maHD\n" +
+"JOIN \n" +
+"    Nhanvien NV ON HD.maNV = NV.maNV\n" +
+"GROUP BY \n" +
+"    NV.hoTen, NV.maNV";
+    @Override
+    public List<ChiTietHoaDon> TongDoanhThu() {
+        return XQuery.getBeanList(ChiTietHoaDon.class, TongDoanhThuSQL);
+    }
+
+    @Override
+    public List<HoaDon> DoanhThuTheoNgay() {
+        return XQuery.getBeanList(HoaDon.class, DoanhThuTheoNgaySQL);
+    }
+
+    @Override
+    public List<NhanVien> DoanhThuNhanVien() {
+        return XQuery.getBeanList(NhanVien.class, DoanhThuNhanVienSQL);
+    }
+    
+}
